57a754eb57932762cdfc45c71885b1a3bd20f633
basfom <jose.follert@sansano.usm.cl>
2016-10-24 23:21:39 -0300

    Version BETA: Toma fotos y las guarda

---
+++ gui/photoboothapp.py
@@ -0,0 +1,144 @@
+# import the necessary packages
+from __future__ import print_function
+from PIL import Image
+from PIL import ImageTk
+import Tkinter as tki
+import threading
+import datetime
+import imutils
+import cv2
+import os
+
+class PhotoBoothApp:
+	def __init__(self, vs):
+		# store the video stream object and output path, then initialize
+		# the most recently read frame, thread for reading frames, and
+		# the thread stop event
+		self.vs = vs
+		self.outputPath = ""
+		self.frame = None
+		self.thread = None
+		self.stopEvent = None
+
+		# initialize the root window and image panel
+		self.root = tki.Tk()
+		self.panel = None
+
+		# create a button, that when pressed, will take the current
+		# frame and save it to file
+		
+		self.btn = tki.Button(self.root, text="Capturar!",
+			command=self.takeSnapshot)
+		self.btn.configure(state="disabled")
+		
+		self.btn2 = tki.Button(self.root, text="Comparar!",
+			command=self.takeSnapshot)
+		self.btn2.configure(state="disabled")
+		
+		self.btn.pack(side="bottom", fill="both", expand="yes", padx=10,
+			pady=10)
+		self.btn2.pack(side="bottom", fill="both", expand="yes", padx=10,
+			pady=10)
+				
+		lb = tki.Label(self.root, text="Nombre:")
+		self.txt = tki.Entry(self.root)
+		self.txt.pack(side="bottom", fill="both", expand="yes", padx=10,
+			pady=10)
+		lb.pack(side="bottom", expand="yes", padx=10,
+			pady=5)
+
+		# start a thread that constantly pools the video sensor for
+		# the most recently read frame
+		self.stopEvent = threading.Event()
+		self.thread = threading.Thread(target=self.videoLoop, args=())
+		self.thread.start()
+
+		# set a callback to handle when the window is closed
+		self.root.wm_title("Check4Face! BETA v1.0")
+		self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+
+	def videoLoop(self):
+		face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_alt.xml')
+		eyes_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
+
+		# DISCLAIMER:
+		# I'm not a GUI developer, nor do I even pretend to be. This
+		# try/except statement is a pretty ugly hack to get around
+		# a RunTime error that Tkinter throws due to threading
+		try:
+			# keep looping over frames until we are instructed to stop
+			while not self.stopEvent.is_set():
+				# grab the frame from the video stream and resize it to
+				# have a maximum width of 300 pixels
+				self.frame = self.vs.read()
+				self.frame = imutils.resize(self.frame, width=300)
+		
+				# OpenCV represents images in BGR order; however PIL
+				# represents images in RGB order, so we need to swap
+				# the channels, then convert to PIL and ImageTk format
+				image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+
+				
+				##AGREGADO
+				gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+				faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+				eyes = eyes_cascade.detectMultiScale(gray, 1.3, 5)
+				
+
+				
+				if len(faces) != 0:
+					for (x,y,w,h) in faces:
+						self.temp_frame = self.frame[y:y+h,x:x+w]
+						cv2.rectangle(image,(x,y),(x+w,y+h),(125,255,0),2)
+						
+					for (x,y,w,h) in eyes:
+    						cv2.circle(image,(x+w/2,y+h/2),w/2,(255,128,0),2)
+					self.btn.configure(state="normal")
+				else:
+					self.btn.configure(state="disabled")
+					
+				image = Image.fromarray(image)
+				image = ImageTk.PhotoImage(image)
+		
+				# if the panel is not None, we need to initialize it
+				if self.panel is None:
+					self.panel = tki.Label(image=image)
+					self.panel.image = image
+					self.panel.pack(side="left", padx=10, pady=10)
+		
+				# otherwise, simply update the panel
+				else:
+					self.panel.configure(image=image)
+					self.panel.image = image
+
+		except RuntimeError, e:
+			print("[INFO] caught a RuntimeError")
+
+	def takeSnapshot(self):
+		# grab the current timestamp and use it to construct the
+		# output path
+		nombre = self.txt.get()
+		nombre = nombre.lower().split(" ")
+		dirr = nombre[0]+"_"+nombre[1]
+		
+		archivos=os.listdir("db/"+dirr)
+		filename = str(len(archivos)) + ".jpg"
+		
+		if os.access("db/"+dirr, os.F_OK):
+			pass
+		else:
+			os.mkdir("db/"+dirr)
+		
+		#p = os.path.sep.join((dir, filename))
+
+		# save the file
+		cv2.imwrite("db/"+dirr+"/"+filename, self.temp_frame.copy())
+		print("[INFO] saved {}".format(filename))
+
+	def onClose(self):
+		# set the stop event, cleanup the camera, and allow the rest of
+		# the quit process to continue
+		print("[INFO] closing...")
+		self.stopEvent.set()
+		self.vs.stop()
+		self.root.quit()
