983bce76669e54e58e898ad056b718c58e72bcbc
Andrés Navarro <Andrs Navarro>
2016-12-03 21:27:49 -0300

    funciones para pasar fotos a 1 y -1 listas

--- funcionesttest.py
+++ funcionesttest.py
@@ -1,12 +1,12 @@
 from PIL import Image
 import os
 import numpy
+import cv2
 def ResizeImages(nombrecarpeta):
     for contador in range(10):
         print contador
         imagen=Image.open("db/"+nombrecarpeta+"/"+str(contador)+".jpg")
         imagen=imagen.resize((300,300),Image.NEAREST)
-        print "creoquefunciona"
         imagen.save("db/"+nombrecarpeta+"/"+str(contador)+".jpg")
 
 
@@ -16,28 +16,23 @@ def ajustar_y_pasar_a_unos(nombrecarpeta):
 
 def ImagentoLista(nombrecarpeta):
     lista_final=[]
-    #GUARDAR LA LISTA DE LISTAS PARA LAS NEURONAS EN UN TXT?
-    #archivo = open(nombrecarpeta+".txt", 'w')
-    for contador in range(10):
-        imagen=Image.open("db/"+nombrecarpeta+"/"+str(contador)+".jpg")
+    for contador in range(10):  
+        imagen=cv2.imread("db/"+nombrecarpeta+"/"+str(contador)+".jpg",0)
+        imagen=cv2.medianBlur(imagen,5)
+        imagen_thresh=cv2.adaptiveThreshold(imagen,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+        cv2.imwrite("db/"+nombrecarpeta+"/"+str(contador)+"thresh.jpg",imagen_thresh) 
+        imagen=Image.open("db/"+nombrecarpeta+"/"+str(contador)+"thresh.jpg")
         matriz = numpy.array(imagen)
         matriz_de_foto=[]
-        matriz_wb=[]
-        for fila in matriz:
-            fila=[]
-            for pixel in fila :
+        for fila in matriz:  
+            for pixel in fila:
                 if pixel<(255/2):
                     matriz_de_foto.append(-1)
-                    fila.append(0)
                 else:
                     matriz_de_foto.append(1)
-                    fila.append(255)
-            matriz_wb.append(fila)
-        arr = numpy.array(matriz_wb)
-        imagenwb=Image.fromarray(arr.clip(0,255).astype('uint8'), 'gray')
-        imagenwb.save(str(contador)+"awa.jpg")
         lista_final.append(matriz_de_foto)
-        #archivo.write(matriz_de_foto)
+    arreglo_para_neuronas=numpy.array(lista_final)
+    #archivo.write(matriz_de_foto)
     #archivo.close()
     #FALTA VER LA WEAAAAA DE COMO QUEDA CON UNOS Y CEROS EN IMAGENCITA XDXDXD
     return True
